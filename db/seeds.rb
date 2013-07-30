a2720cdd813968b61cd4ca506d3d109e47fd4d58
Eamon <eamon.skelly@sage.com>
2013-07-30 11:17:28 +0100

    New seeds for 2013 to 2015

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,33 @@
-HolidayStatus.create([{:status => 'Pending'}, {:status => 'Authorised'}, {:status => 'Cancelled'}, {:status => 'Taken'}])
-UserType.create([{:name=>"Standard"}, {:name=>"Manager"}])
+#2013 holidays
+BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2011-01-01'},
+                    {:name=>'Good Friday', :date_of_hol=>'2011-03-29'},
+                    {:name=>'Easter Monday', :date_of_hol=>'2011-04-01'},
+                    {:name=>'Early May Bank Holiday', :date_of_hol=>'2011-05-06'},
+                    {:name=>'Spring Bank Holiday', :date_of_hol=>'2011-05-27'},
+                    {:name=>'Summer Bank Holiday', :date_of_hol=>'2011-08-26'},
+                    {:name=>'Christmas Day', :date_of_hol=>'2011-12-25'},
+                    {:name=>'Boxing Day', :date_of_hol=>'2011-12-26'}])
 
-#2011 holidays
-BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2011-01-03'},
-                    {:name=>'Good Friday', :date_of_hol=>'2011-04-22'},
-                    {:name=>'Easter Monday', :date_of_hol=>'2011-04-25'},
-                    {:name=>'Royal Wedding', :date_of_hol=>'2011-04-29'},
-                    {:name=>'Early May Bank Holiday', :date_of_hol=>'2011-05-02'},
-                    {:name=>'Spring Bank Holiday', :date_of_hol=>'2011-05-30'},
-                    {:name=>'Summer Bank Holiday', :date_of_hol=>'2011-08-29'},
-                    {:name=>'Christmas Day', :date_of_hol=>'2011-12-26'},
-                    {:name=>'Boxing Day', :date_of_hol=>'2011-12-27'}])
+# 2014 Holidays
+BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2011-01-01'},
+                    {:name=>'Good Friday', :date_of_hol=>'2011-04-18'},
+                    {:name=>'Easter Monday', :date_of_hol=>'2011-04-21'},
+                    {:name=>'Early May Bank Holiday', :date_of_hol=>'2011-05-05'},
+                    {:name=>'Spring Bank Holiday', :date_of_hol=>'2011-05-26'},
+                    {:name=>'Summer Bank Holiday', :date_of_hol=>'2011-08-25'},
+                    {:name=>'Christmas Day', :date_of_hol=>'2011-12-25'},
+                    {:name=>'Boxing Day', :date_of_hol=>'2011-12-26'}])
 
-#2012 holidays
-BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2012-01-02'},
-                    {:name=>'Good Friday', :date_of_hol=>'2012-04-06'},
-                    {:name=>'Easter Monday', :date_of_hol=>'2012-04-09'},
-                    {:name=>'Early May Bank Holiday', :date_of_hol=>'2012-05-07'},
-                    {:name=>'Spring Bank Holiday', :date_of_hol=>'2012-06-04'},
-                    {:name=>'Diamond Jubilee', :date_of_hol=>'2012-06-05'},
-                    {:name=>'Summer Bank Holiday', :date_of_hol=>'2012-08-27'},
-                    {:name=>'Christmas Day', :date_of_hol=>'2012-12-25'},
-                    {:name=>'Boxing Day', :date_of_hol=>'2012-12-26'}])
+# 2015 Holidays
+BankHoliday.create([{:name=>'New Years Day', :date_of_hol=>'2011-01-01'},
+                    {:name=>'Good Friday', :date_of_hol=>'2011-04-03'},
+                    {:name=>'Easter Monday', :date_of_hol=>'2011-04-06'},
+                    {:name=>'Early May Bank Holiday', :date_of_hol=>'2011-05-04'},
+                    {:name=>'Spring Bank Holiday', :date_of_hol=>'2011-05-25'},
+                    {:name=>'Summer Bank Holiday', :date_of_hol=>'2011-08-31'},
+                    {:name=>'Christmas Day', :date_of_hol=>'2011-12-25'},
+                    {:name=>'Boxing Day', :date_of_hol=>'2011-12-28'}])
 
-HolidayYear.create([{:description => "Oct 2010 to Sept 2011", :date_start => "2010-10-01", :date_end=> "2011-09-30"},
-                    {:description => "Oct 2011 to Sept 2012", :date_start => "2011-10-01", :date_end=> "2012-09-30"},
-                    {:description => "Oct 2012 to Sept 2013", :date_start => "2012-10-01", :date_end=> "2013-09-30"}])
+HolidayYear.create([{:description => "Oct 2013 to Sept 2014", :date_start => "2013-10-01", :date_end=> "2014-09-30"},
+                    {:description => "Oct 2014 to Sept 2015", :date_start => "2014-10-01", :date_end=> "2015-09-30"},
+                    {:description => "Oct 2015 to Sept 2016", :date_start => "2015-10-01", :date_end=> "2016-09-30"}])
