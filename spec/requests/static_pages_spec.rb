e24017c52babe2e7b644e24fe8423ab211efb957
Liam Lagay <liam.lagay@sage.com>
2014-09-25 11:06:08 +0100

    Added lost password functionality, along with updates to confirmation + unlocking of account views to bring them into line with the styles of the updated app

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,6 +17,22 @@ describe "Static pages" do
     it { expect(subject).to_not have_title('| Home') }
     it { expect(subject).to have_selector('div.alert.alert-alert', text: I18n.t('devise.failure.unauthenticated')) }
 
+    describe 'clicking on Sign up now' do
+      before { click_link 'Sign up now!' }
+
+      it_should_behave_like 'all static pages'
+
+      it { expect(subject).to have_content('Sign up') }
+    end
+
+    describe 'clicking on the Forgotten password link' do
+      before { click_link 'Reset password' }
+
+      it_should_behave_like 'all static pages'
+
+      it { expect(subject).to have_content('Forgot your password?') }
+    end
+
   end
 end
 
