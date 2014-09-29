163cbcbb69f458520addfb07ff51bf8304e9c9e6
Liam Lagay <liam.lagay@sage.com>
2014-09-29 15:51:59 +0100

    Removing unused code and adding extra rspec tests

--- app/models/holiday_status.rb
+++ app/models/holiday_status.rb
@@ -3,19 +3,4 @@ class HolidayStatus < ActiveRecord::Base
 
   validates :status, presence: true
   attr_accessible :status
-
-  scope :pending_only, -> {where('status = "Pending"')}
-
-  scope :user_statuses, lambda { |user_type_name|
-     if user_type_name == "Manager"
-       all
-     else
-       where('status == "Pending" and status == "Cancelled"')
-     end
-     }
-
-  def self.conditions_mgr
-    conditions=[]
-    conditions << self.status !="Pending" and self.status != "Cancelled"
-  end
 end
--- spec/models/bank_holidays_spec.rb
+++ spec/models/bank_holidays_spec.rb
@@ -1,13 +1,44 @@
 require 'spec_helper'
 
 describe BankHoliday do
-  describe :validations do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:date_of_hol) }
+  let(:bank_holiday) { BankHoliday.create(name: 'Christmas Day', date_of_hol: '25 Dec 2014') }
+
+  subject { bank_holiday }
+
+  it { expect(subject).to be_valid }
+
+  describe 'pulling back the correct number of bank holidays' do
+    before do
+      @date_from = DateTime.new(2014, 12, 20)
+      @date_to = DateTime.new(2014, 12, 26)
+    end
+    it { expect(BankHoliday.in_between(@date_from, @date_to).size).to eq(2) }
   end
 
-  describe :security do
-    it { should allow_mass_assignment_of(:name) }
-    it { should allow_mass_assignment_of(:date_of_hol) }
+  describe 'pulling back zero bank holidays correctly' do
+    before do
+      @date_from = DateTime.new(2014, 12, 23)
+      @date_to = DateTime.new(2014, 12, 24)
+    end
+
+    it { expect(BankHoliday.in_between(@date_from, @date_to).size).to eq(0) }
+  end
+
+  describe 'supplying an invalid Bank Holiday name' do
+    before do
+      subject.name = nil
+      subject.save
+    end
+
+    it { expect(subject).to_not be_valid }
+  end
+
+  describe 'supplying an invalid date of holiday' do
+    before do
+      subject.date_of_hol = nil
+      subject.save
+    end
+
+    it { expect(subject).to_not be_valid }
   end
 end
